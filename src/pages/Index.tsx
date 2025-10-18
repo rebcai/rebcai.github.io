e65989061dbb47b510c91a6affab92083201a6d7
rebcai <146408859+rebcai@users.noreply.github.com>
2025-10-18 10:56:11 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,9 @@ import { AutoCarousel } from "@/components/AutoCarousel";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background p-8">
-      <div className="w-full max-w-6xl mx-auto space-y-12">
+      <div className="w-full max-w-6xl mx-auto space-y-10">
         <div className="space-y-4 text-center">
-          <h1 className="text-2xl name-title font-light">REBECCA CAI</h1>
+          <h1 className="text-2xl name-title font-light">rebecca cai</h1>
           <h2 className="text-lg email-subtitle font-light pb-2">
             email@placeholder
           </h2>
